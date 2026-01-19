038c48b96a9fe8b2f091e4658a07ec2d7fc15353
Maksym Malyi <malyimaksym37@gmail.com>
2026-01-19 07:50:04 +0200

    first task

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    var personBob = new Person("Bob",25,"driver");
+    var personJane = new Person("Jane",28,"manager");
+    var personJohn = new Person("John",38,"developer");
+    Person[] arrayOfPersons = {personJohn, personBob, personJane};
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    for(Person person : arrayOfPersons) {
+        person.getInformation();
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,31 @@
+/*
+Створіть Java програму з використанням класів та конструкторів, яка виконує наступні завдання:
+
+    Створіть клас Person, який представляє особу з такими характеристиками: 
+    ім'я, вік та професія. Оголосіть поля для цих характеристик та створіть конструктор класу,
+    який дозволяє встановити значення цих полів при створенні об'єкта класу Person.
+    
+    Створіть об'єкти класу Person для трьох різних осіб, використовуючи конструктор класу Person.
+     Виведіть інформацію про цих осіб на екран.
+    
+    Додайте метод в клас Person, який дозволяє встановити нову професію для особи. 
+    
+    Використайте цей метод для зміни професії одного з об'єктів Person, а потім виведіть оновлену інформацію про цю особу на екран.
+    Залийте виконаний проект на свій GitHub репозиторій, посилання на який зазначте в LMS. 
+* */
+public class Person {
+    private String name;
+    private int age;
+    private String profession;
+    public Person ( String personName, int personAge, String personProfession) {
+        this.name = personName;
+        this.age = personAge;
+        this.profession = personProfession;
+    }
+    public void getInformation (){
+        String  templateForOutput = """
+              ім'я: %s, вік: %d, професія: %s;
+             """;
+        System.out.printf(templateForOutput,name,age,profession);
+    }
+}
